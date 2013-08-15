38c5d6891243bb1e8c4a32e7b8e31de6abf0bc7d
Tim Sheridan <tghs@tghs.net>
2013-08-16 00:05:33 +0100

    Add support for passing user@host as an argument.

--- sshfav.sh
+++ sshfav.sh
@@ -6,9 +6,14 @@
 # Connect to your favourite SSH locations by making symlinks to this script.
 # Filenames should be of the form user@host. A username will be prompted for
 # if only a hostname is provided.
+# Alternatively, the connection details can be passed as an argument.
 
 set -eu
-INVOKE_NAME=$(basename `echo $0`)
+if [ $# -gt 0 ]; then
+	INVOKE_NAME="$1"
+else
+	INVOKE_NAME=$(basename `echo $0`)
+fi
 
 # Extract strings from symlink
 SSH_HOST="${INVOKE_NAME#*@}"
