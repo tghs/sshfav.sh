19de3bb5b420e5edd4c038e10902294a7ec8bf58
Tim Sheridan <tghs@tghs.net>
2013-09-01 20:58:01 +0100

    Add unit tests for user/host/port parsing.

--- test.sh
+++ test.sh
@@ -31,6 +31,21 @@ test_str_neq() {
 . ./sshfav.sh -h &> /dev/null
 
 # Test cases
-# TODO
+
+test_str_eq "`user_from_connection_spec 'example.com'`" ""
+test_str_eq "`host_from_connection_spec 'example.com'`" "example.com"
+test_str_eq "`port_from_connection_spec 'example.com'`" "22"
+
+test_str_eq "`user_from_connection_spec 'user@example.com'`" "user"
+test_str_eq "`host_from_connection_spec 'user@example.com'`" "example.com"
+test_str_eq "`port_from_connection_spec 'user@example.com'`" "22"
+
+test_str_eq "`user_from_connection_spec 'example.com:1234'`" ""
+test_str_eq "`host_from_connection_spec 'example.com:1234'`" "example.com"
+test_str_eq "`port_from_connection_spec 'example.com:1234'`" "1234"
+
+test_str_eq "`user_from_connection_spec 'user@example.com:1234'`" "user"
+test_str_eq "`host_from_connection_spec 'user@example.com:1234'`" "example.com"
+test_str_eq "`port_from_connection_spec 'user@example.com:1234'`" "1234"
 
 #eof
