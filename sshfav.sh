e1eb9351de58ef876b36f5a6f3fe1c1af2132f8b
Tim Sheridan <tghs@tghs.net>
2013-09-01 19:56:06 +0100

    Wrap script contents in main() function to assist unit testing.

--- sshfav.sh
+++ sshfav.sh
@@ -10,26 +10,31 @@
 # Alternatively, the connection details can be passed as an argument.
 
 set -eu
-if [ $# -gt 0 ]; then
-	INVOKE_NAME="$1"
-else
-	INVOKE_NAME=$(basename `echo $0`)
-fi
 
-# Extract strings from symlink
-SSH_HOST="${INVOKE_NAME#*@}"
-SSH_USER="${INVOKE_NAME%@*}"
-SSH_PORT="${SSH_HOST#*:}"
-SSH_HOST="${SSH_HOST%:*}"
-if [ "$SSH_PORT" == "$SSH_HOST" ]; then
-    SSH_PORT="22"
-fi
+main() {
+	if [ $# -gt 0 ]; then
+		INVOKE_NAME="$1"
+	else
+		INVOKE_NAME=$(basename `echo $0`)
+	fi
+	
+	# Extract strings from symlink
+	SSH_HOST="${INVOKE_NAME#*@}"
+	SSH_USER="${INVOKE_NAME%@*}"
+	SSH_PORT="${SSH_HOST#*:}"
+	SSH_HOST="${SSH_HOST%:*}"
+	if [ "$SSH_PORT" == "$SSH_HOST" ]; then
+	    SSH_PORT="22"
+	fi
+	
+	# Set remote username to local username if none was provided
+	while [ "$SSH_USER" == "$SSH_HOST" ] || [ "$SSH_USER" == "" ]; do
+	    read -p "User: " SSH_USER
+	done
+	
+	ssh -p $SSH_PORT $SSH_USER@$SSH_HOST
+}
 
-# Set remote username to local username if none was provided
-while [ "$SSH_USER" == "$SSH_HOST" ] || [ "$SSH_USER" == "" ]; do
-    read -p "User: " SSH_USER
-done
-
-ssh -p $SSH_PORT $SSH_USER@$SSH_HOST
+main "$@"
 
 #eof
